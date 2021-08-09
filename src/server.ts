7809b36e0bf8a7c2e77caab4bc7bd48bee3f0c7b
orodrigoh <rodrigo.hss@outlook.com>
2021-08-09 19:44:06 -0300

    heroku

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
-
+import cors from 'cors'
 import express, {Request, Response, NextFunction} from "express";
 import 'express-async-errors';
-import cors from 'cors'
+
 import { router } from "./routes";
 
 import "./database";
